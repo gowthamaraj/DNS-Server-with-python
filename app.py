919504d0a82b0bd3d2c572d49bed85680e26f511
gowthamaraj <gowthamaraj360@gmail.com>
2020-11-12 22:47:10 +0530

    Get Headers

--- app.py
+++ app.py
@@ -3,8 +3,27 @@ import socket
 PORT = 53
 IP = "127.0.0.1"
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.bind((IP,PORT))
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((IP, PORT))
+
+
+def get_flags(data):
+    pass
+
+
+def build_response(data):
+
+    # Transaction ID
+    TransactionID = data[0:2]
+    TID = ''
+    for byte in TransactionID:
+        TID += hex(byte)[2:]
+
+    # Get the flags
+    Flags = get_flags(data[2:4])
+
 
 while True:
     data, addr = sock.recvfrom(512)
+    r = build_response(data)
+    sock.sendto(r, addr)
